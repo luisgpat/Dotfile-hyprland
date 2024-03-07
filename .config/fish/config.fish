239bd4e91ed4953c443b1df6e5114b71bf3126f1
luisgpat <luisg.pat@gmail.com>
2024-03-07 14:25:31 -0500

    Added a lot

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    pfetch
-    # Commands to run in interactive sessions can go here
-    set -g fish_greeting
-    set EDITOR nvim
-    set TERM kitty
-end
-fish_add_path /home/luigo/.spicetify
--- .config/rclone/syncscript.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Copy from Google drive to Local Folder
-rclone bisync gdrive_p: $HOME/gdrive/Personal/ --resync --progress --log-file .config/rclone.log
